1761bba2872f2d97cbd5783bd7d3c21a11c3a87b
Mattias Rost <rrostt@gmail.com>
2022-11-26 22:36:57 +0100

    Fix so Sebastian can't cheat.

--- pages/api/submit-answer.js
+++ pages/api/submit-answer.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   const guess3 = req.body.guess3.toLowerCase()
   const question = getQuestion(questionId)
   const answers = question.people.map(p => p.toLowerCase())
-  const correct = [guess1, guess2, guess3].filter(answer => answers.includes(answer)).length == 3
+  const correct = [...new Set([guess1, guess2, guess3])].filter(answer => answers.includes(answer)).length == 3
 
   res.status(200).json({
     correct,
