2d738715b9f8cfd0abd403b8d9bdb7461697aebf
Sebastian Andreasson <sebastianandreasson@MacBook-Pro.local>
2022-11-29 19:52:06 +0100

    add share preview to index

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Button, Center, Flex, Text, Title } from '@mantine/core'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 
@@ -12,39 +13,60 @@ export default function Index() {
   }
 
   return (
-    <Center>
-      <Flex direction="column" gap={0} align="center" mt={128}>
-        <Title
-          order={1}
-          size={96}
-          sx={{
-            '@media (max-width: 755px)': {
-              fontSize: '48px',
-            },
-          }}
-        >
-          WHO AM <span style={{ fontSize: 32 }}>A</span>I?
-        </Title>
-        <Title
-          order={2}
-          weight={400}
-          ta="center"
-          sx={{
-            '@media (max-width: 755px)': {
-              fontSize: '18px',
-            },
-          }}
-        >
-          Guess which <strong>three</strong> people an AI generated image is.
-        </Title>
-        <Text mt={64} maw="500px" ta="center">
-          When you press start you will get a generated image of three famous
-          people merged together, can you guess who they are?
-        </Text>
-        <Button mt="lg" size="lg" onClick={start} loading={loading}>
-          Start
-        </Button>
-      </Flex>
-    </Center>
+    <>
+      <Head>
+        <title>Who am aI?</title>
+        <meta
+          property="description"
+          content="Guess which three people an AI generated image is."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Who am aI?" />
+        <meta
+          property="og:description"
+          content="Guess which three people an AI generated image is."
+        />
+        <meta
+          property="og:image"
+          content="https://whoamai.appadem.in/images/share.png"
+        />
+        <meta property="og:image:width" content={512} />
+        <meta property="og:image:height" content={502} />
+      </Head>
+      <Center>
+        <Flex direction="column" gap={0} align="center" mt={128}>
+          <Title
+            order={1}
+            size={96}
+            sx={{
+              '@media (max-width: 755px)': {
+                fontSize: '48px',
+              },
+            }}
+          >
+            WHO AM <span style={{ fontSize: 32 }}>A</span>I?
+          </Title>
+          <Title
+            order={2}
+            weight={400}
+            ta="center"
+            sx={{
+              '@media (max-width: 755px)': {
+                fontSize: '18px',
+              },
+            }}
+          >
+            Guess which <strong>three</strong> people an AI generated image is.
+          </Title>
+          <Text mt={64} maw="500px" ta="center">
+            When you press start you will get a generated image of three famous
+            people merged together, can you guess who they are?
+          </Text>
+          <Button mt="lg" size="lg" onClick={start} loading={loading}>
+            Start
+          </Button>
+        </Flex>
+      </Center>
+    </>
   )
 }
