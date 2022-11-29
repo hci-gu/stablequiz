cbf9576d731ede365b31cff732425c426da8185e
Mattias Rost <rrostt@gmail.com>
2022-11-29 20:30:08 +0100

    Remove copy link button

--- pages/question/[id].js
+++ pages/question/[id].js
@@ -189,7 +189,7 @@ export default function Question({ question, people = [] }) {
             )}
           </Flex>
 
-          <CopyLinkButton />
+          {/* <CopyLinkButton /> */}
         </Flex>
       </Center>
     </>
